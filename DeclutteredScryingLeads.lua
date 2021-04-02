60bfad7eda6a29b3daead380c22827b8f5fe5285
Kawamonkey <79089984+kawamonkey-eso@users.noreply.github.com>
2021-04-02 20:25:33 +0100

    use correct method to identify starter leads

--- DeclutteredScryingLeads.lua
+++ DeclutteredScryingLeads.lua
@@ -1,5 +1,5 @@
 local function MatchAllAntiquitiesWithLeads(antiquityData)
-    return antiquityData:IsInProgress() or (antiquityData:MeetsLeadRequirements() and antiquityData:GetDifficulty() ~= 1)
+    return antiquityData:IsInProgress() or antiquityData:HasLead()
 end
 
 ZO_SCRYABLE_ANTIQUITY_ALL_LEADS_SUBCATEGORY_DATA:SetAntiquityFilterFunction(MatchAllAntiquitiesWithLeads)
